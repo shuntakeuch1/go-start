f8e32dca2a580db7b6f57eec6813d06ba0b9fda3
shuntakeuch1 <takeuchishun89@gmail.com>
2019-05-08 00:56:28 +0900

    引数でポートを取得できるように変更

--- chat/main.go
+++ chat/main.go
@@ -21,7 +21,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r) // 本当は戻り値をチェックすべき
+	t.templ.Execute(w, r)
 }
 
 func main() {
