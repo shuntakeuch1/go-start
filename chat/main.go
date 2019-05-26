acec79985a930aebfdf86dd366fcd4a3a1e64339
shuntakeuch1 <takeuchishun89@gmail.com>
2019-05-27 07:45:03 +0900

    パス処理の追加

--- chat/main.go
+++ chat/main.go
@@ -35,6 +35,8 @@ func main() {
 
 	// http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// チャットルームを開始します
 	go r.run()
